7ac0818ecaecdb213bd12cfd5a295422056f8236
Neha Agrawal <88786146+iamproaf@users.noreply.github.com>
2023-04-15 16:08:20 +0530

    Update extraction.py

--- cutie/modules/helper_funcs/extraction.py
+++ cutie/modules/helper_funcs/extraction.py
@@ -1,7 +1,7 @@
 from typing import List, Optional
 
-from Tianabot import LOGGER
-from Tianabot.modules.users import get_user_id
+from cutie import LOGGER
+from cutie.modules.users import get_user_id
 from telegram import Message, MessageEntity
 from telegram.error import BadRequest
 
