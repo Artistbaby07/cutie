9081d89d971ac29f471cf824c32a691b9e671184
Neha Agrawal <88786146+iamproaf@users.noreply.github.com>
2023-04-16 11:48:01 +0530

    Update approve.py

--- cutie/modules/approve.py
+++ cutie/modules/approve.py
@@ -225,6 +225,6 @@ dispatcher.add_handler(APPROVAL)
 dispatcher.add_handler(UNAPPROVEALL)
 dispatcher.add_handler(UNAPPROVEALL_BTN)
 
-__mod_name__ = "☯︎ 𝘼𝙥𝙥𝙧𝙤𝙫𝙚 ☯︎"
+__mod_name__ = "Approve"
 __command_list__ = ["approve", "unapprove", "approved", "approval"]
 __handlers__ = [APPROVE, DISAPPROVE, APPROVED, APPROVAL]
